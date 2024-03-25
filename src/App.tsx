b5ffc6b4e4784939c85c2b0076e42a96bffcdf5f
kirankumal <kiran.kumal714@gmail.com>
2024-03-25 17:57:19 +0545

    :tada: Inititalize starter kit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-blue-500 text-center text-3xl flex justify-center items-center h-screen">
+      Hello World
+    </h1>
+  );
+}
+
+export default App;
