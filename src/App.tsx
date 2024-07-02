3c19732343e81860496365863af40b85bcbe76b6
Kiran Kumal <kiran.kumal@webo.digital>
2024-07-02 16:24:44 +0545

    feat: integrate husky

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <button onClick={() => toast('My first toast')} className="text-blue-500 text-center text-3xl font-bold flex justify-center items-center h-screen">
-        Give me a toastdfg
+        Give me a toast
       </button>
       <Toaster />
     </div>
